38f33110ba64efda69098ec00b9ca3dde1cee691
Nick Sutterer <apotonick@gmail.com>
2009-06-30 23:31:59 +0200

    fixed the default url to the YUI /build repository.

--- lib/yui/configuration.rb
+++ lib/yui/configuration.rb
@@ -1,6 +1,6 @@
 module YUI
   class Configuration
-    @@build_url = "yui.yahooapis.com/2.7.0/build/"
+    @@build_url = "http://yui.yahooapis.com/2.7.0/build/"
     cattr_accessor :build_url
   end
 end
